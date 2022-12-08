5fe7dc3be6836f9f2ad1a66114445cd878d150fd
Meetakshi Setiya <meetakshisty4@gmail.com>
2022-12-08 07:29:40 +0530

    feat: add search api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import authMiddleware from "./src/middleware/authMiddleware.js";
 import images from "./src/images/index.js";
 import albums from "./src/albums/index.js";
+import search from "./src/search/index.js"
 import bodyParser from "body-parser";
 
 const app = express();
@@ -16,8 +17,19 @@ app.use((req, res, next) => jsonParser(req, res, next));
 
 app.use("/images", images);
 app.use("/albums", albums);
+app.use("/search", search);
 
 const port = process.env.PORT || 6542;
+
+app.get("/", async (req, res, next) => {
+	res.status(200).json({ status: "api running" });
+});
+
+// Catch all uncaught routes and 404
+app.all("*", (req, res, next) => {
+	res.status(404).json({ status: "route not found for request" });
+});
+
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
